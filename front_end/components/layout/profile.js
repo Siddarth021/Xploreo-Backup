af95d5e7898452e7f6968654e6e0c06a02636a68
Sriram <sriram.k24@iiits.in>
2026-03-27 05:18:30 +0000

    #updated app.js

--- front_end/components/layout/profile.js
+++ front_end/components/layout/profile.js
@@ -1,23 +1,33 @@
 export function getProfile(user){
     return `
-        <div class="profile" onclick="toggleProfileMenu()">
+        <div class="profile">
             <img src="../components/ui/profile.png" alt="user" class="profile-img"/>
             <span class="profile-name">${user.name}</span>
-            <div id="profileDropdown" class="profile-dropdown hidden">
-            <p>${user.name}</p>
-            <ol>
-             <li>Profile</li>
-             <li>Support</li>
-            </ol>
-            <button onclick="logout()">Logout</button>
+
+            <div class="profile-dropdown hidden">
+                <p>${user.name}</p>
+                <ol>
+                    <li>Profile</li>
+                    <li>Support</li>
+                </ol>
+                <button onclick="logout()">Logout</button>
             </div>
-        </div>`;
+        </div>
+    `;
 }
 
-window.toggleProfileMenu = function () {
-  const menu = document.getElementById("profileDropdown");
-  menu.classList.toggle("hidden");
-};
+window.addEventListener("click", function (e) {
+    const profile = document.querySelector(".profile");
+    const dropdown = document.querySelector(".profile-dropdown");
+
+    if (!profile || !dropdown) return;
+
+    if (profile.contains(e.target)) {
+        dropdown.classList.toggle("hidden");
+    } else {
+        dropdown.classList.add("hidden");
+    }
+});
 
 window.logout = function () {
   localStorage.removeItem("currentUser");

05fffc56585ee5251d419668f2c0d11eaa7561ad
Sriram <sriram.k24@iiits.in>
2026-03-27 13:19:31 +0000

    updated dashboard and render logic

--- front_end/js/renderpages.js
+++ front_end/js/renderpages.js
@@ -4,15 +4,19 @@ import { renderdasboard } from "./dashboard.js";
 export function renderPageContent(user) {
     const path = window.location.pathname.split("/").pop();
 
-    // ===== GUIDE =====
     if (user.role === "guide" && path === "dashboard.html") {
+<<<<<<< HEAD
         renderdasboard(user);
+=======
+        renderStats("stats-section");
+>>>>>>> f53fbbb (updated dashboard and render logic)
         document.getElementById("admin-dashboard").style.display = "none";
-    }
 
-    // ===== ADMIN =====
-    if (user.role === "superadmin" && path === "dashboard.html") {
+    } else if (user.role === "superadmin" && path === "dashboard.html") {
         renderAdminDashboard("admin-dashboard");
         document.getElementById("main").style.display = "none";
+
+    } else {
+        console.log("Unknown role:", user.role);
     }
 }

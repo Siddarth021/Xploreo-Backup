ab0d9fa9f4bed983f07dd5a850e835fd76cfda84
Ssk7126 <sreekarkothapalli@gmail.com>
2026-03-27 17:21:20 +0530

    ..

--- front_end/js/renderpages.js
+++ front_end/js/renderpages.js
@@ -1,22 +1,18 @@
-import { renderStats } from "./modules/stat-cards.js";
 import { renderAdminDashboard } from "./adminDashboard.js";
+import { renderdasboard } from "./dashboard.js";
 
 export function renderPageContent(user) {
     const path = window.location.pathname.split("/").pop();
 
     // ===== GUIDE =====
     if (user.role === "guide" && path === "dashboard.html") {
-        renderStats("stats-section");
-
-        // hide admin
+        renderdasboard(user);
         document.getElementById("admin-dashboard").style.display = "none";
     }
 
     // ===== ADMIN =====
     if (user.role === "superadmin" && path === "dashboard.html") {
         renderAdminDashboard("admin-dashboard");
-
-        // hide guide layout
         document.getElementById("main").style.display = "none";
     }
 }

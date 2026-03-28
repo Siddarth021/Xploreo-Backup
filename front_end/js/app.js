436c200dc6a2caa28c15aea3838a0716d0a16d37
Ssk7126 <sreekarkothapalli@gmail.com>
2026-03-28 08:25:40 +0530

    complete resolve

--- front_end/js/app.js
+++ front_end/js/app.js
@@ -32,7 +32,7 @@ document.addEventListener("DOMContentLoaded", () => {
     let currentUser = JSON.parse(localStorage.getItem("currentUser"));
     
     if (!currentUser) {
-        currentUser = users.find(u => u.id === "101");
+        currentUser = users.find(u => u.id === "001");
         localStorage.setItem("currentUser", JSON.stringify(currentUser));
     }
     renderNavbar(currentUser);

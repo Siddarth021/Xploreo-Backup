ce98519ce1635796219fc7428b4f8737f5afc0f0
Ssk7126 <sreekarkothapalli@gmail.com>
2026-04-02 09:45:38 +0530

    ..

--- front_end/components/layout/navlinks_landing.js
+++ front_end/components/layout/navlinks_landing.js
@@ -1,6 +1,6 @@
 export const landingLinks = [
-    { label: "Home", href: "../index.html" },
-    { label: "Tours", href: "../pages/tours.html" },
-    { label: "Partners", href: "../pages/partners.html" },
-    { label: "Support", href: "../pages/support.html" }
+    { label: "Home", href: "/index.html" },
+    { label: "Tours", href: "/pages/tours.html" },
+    { label: "Partners", href: "/pages/partners.html" },
+    { label: "Support", href: "/pages/support.html" }
 ];

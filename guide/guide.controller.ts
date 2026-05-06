893f8e16e9fea4dea38207d5a8000da9ef34ea71
Siddarth021 <gundetisiddarth@gmail.com>
2026-05-06 11:53:05 +0530

    Delete guide directory

--- guide/guide.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('guide')
-export class GuideController {}
--- guide/guide.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { GuideService } from './guide.service';
-import { GuideController } from './guide.controller';
-
-@Module({
-  providers: [GuideService],
-  controllers: [GuideController]
-})
-export class GuideModule {}
